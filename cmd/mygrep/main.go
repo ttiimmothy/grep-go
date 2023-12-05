547f8a2688d1185c07805e75b1b66583f2a0b68e
ttiimmothy <dinosauli2006@gmail.com>
2023-12-05 08:33:58 -0500

    feat: 5th stage

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -1,24 +1,96 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
-	"unicode/utf8"
+	"strings"
+	"unicode"
 )
 
-func matchLine(line []byte, pattern string) (bool, error) {
-	if utf8.RuneCountInString(pattern) < 1 {
-		return false, fmt.Errorf("unsupported pattern: %q", pattern)
+func IsDigit(s string) bool {
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
 	}
-	var ok bool
-	if pattern == "\\d" {
-		ok = bytes.ContainsAny(line, "0123456789")
+	return true
+}
+
+func IsLetter(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func matchLine(line string, pattern string, matchStart bool) (bool, error) {
+	if len(pattern) == 0 {
+		return true, nil
+	}
+	if len(line) == 0 {
+		return len(pattern) == 0, nil
+	}
+	if strings.HasPrefix(pattern, "^") {
+		return matchLine(line, pattern[1:], true)
+	}
+	if strings.HasPrefix(pattern, "[") {
+		end := strings.IndexByte(pattern, ']')
+		charset := pattern[1:end]
+		if charset[0] == '^' {
+			for _, c := range charset {
+				if strings.HasPrefix(line, string(c)) {
+					return false, nil
+				}
+			}
+			return matchLine(line[1:], pattern[end+1:], matchStart)
+		} else {
+			for _, c := range charset {
+				if strings.HasPrefix(line, string(c)) {
+					result, _ := matchLine(line[1:], pattern[end+1:], true)
+					if result {
+						return true, nil
+					}
+				}
+			}
+		}
+		return false, nil
+	}
+
+	char := string(line[0])
+	if strings.HasPrefix(pattern, "\\w") {
+		if IsLetter(char) || IsDigit(char) {
+			return matchLine(line[1:], pattern[2:], matchStart)
+		} else {
+			if matchStart {
+				return false, nil
+			} else {
+				return matchLine(line[1:], pattern, matchStart)
+			}
+		}
+	} else if strings.HasPrefix(pattern, "\\d") {
+		if IsDigit(char) {
+			return matchLine(line[1:], pattern[2:], matchStart)
+		} else {
+			if matchStart {
+				return false, nil
+			} else {
+				return matchLine(line[1:], pattern, matchStart)
+			}
+		}
 	} else {
-		ok = bytes.ContainsAny(line, pattern)
+		if string(pattern[0]) == char {
+			return matchLine(line[1:], pattern[1:], matchStart)
+		} else {
+			if matchStart {
+				return false, nil
+			} else {
+				return matchLine(line[1:], pattern, matchStart)
+			}
+		}
 	}
-	return ok, nil
 }
 
 func main() {
@@ -35,7 +107,7 @@ func main() {
 		os.Exit(2)
 	}
 
-	ok, err := matchLine(line, pattern)
+	ok, err := matchLine(string(line), pattern, false)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(2)
